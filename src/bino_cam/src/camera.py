985707aa753ecad8bc1353e50f3a31e2c402f393
Andrew McDonald <mcdonalda1993@gmail.com>
2014-10-24 17:02:17 +0100

    Both cameras capturing.

--- src/bino_cam/src/camera.py
+++ src/bino_cam/src/camera.py
@@ -1,28 +1,31 @@
 import numpy as np
 import cv2
+import sys
+
+def checkCamera(cam):
+	if(cam.isOpened()==False):
+		sys.exit("A camera is not detected")
+
 
 cap = cv2.VideoCapture(0)
-
-print cap.set(3, 1920)
-print cap.set(4, 1080)
-
-print cap.getAllProperties()
+checkCamera(cap)
+cap2 = cv2.VideoCapture(1)
+checkCamera(cap2)
 
 while(True):
+    
     # Capture frame-by-frame
     ret, frame = cap.read()
-
-    # Our operations on the frame come here
-    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    ret2, frame2 = cap2.read()
 
     # Display the resulting frame
     cv2.imshow('frame', frame)
-    
-   
+    cv2.imshow('frame2', frame2)
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 # When everything done, release the capture
 cap.release()
+cap2.release()
 cv2.destroyAllWindows()
