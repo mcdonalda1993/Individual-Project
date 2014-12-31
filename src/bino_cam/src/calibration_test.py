ec4ee272b9dead56673e0d94c627e0e1a10e3e5d
Andrew McDonald <mcdonalda1993@gmail.com>
2014-12-31 02:56:13 +0000

    Fixed example program to work with live feed.

--- src/bino_cam/src/calibration_test.py
+++ src/bino_cam/src/calibration_test.py
@@ -13,25 +13,30 @@ objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
 
-images = glob.glob('*.jpg')
+patternSize = (7,6)
 
-for fname in images:
-    img = cv2.imread(fname)
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+cam = cv2.VideoCapture(0)
 
-    # Find the chess board corners
-    ret, corners = cv2.findChessboardCorners(gray, (7,6),None)
+while(len(imgpoints) < 5):
+	ret1, img = cam.read()
+	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	
+	ret = False
+	# Find the chess board corners
+	ret, corners = cv2.findChessboardCorners(gray, patternSize, None)
 
-    # If found, add object points, image points (after refining them)
-    if ret == True:
-        objpoints.append(objp)
+	# If found, add object points, image points (after refining them)
+	if ret == True:
+		objpoints.append(objp)
 
-        corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
-        imgpoints.append(corners2)
+		cv2.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria)
+		imgpoints.append(corners)
 
-        # Draw and display the corners
-        img = cv2.drawChessboardCorners(img, (7,6), corners2,ret)
-        cv2.imshow('img',img)
-        cv2.waitKey(500)
+		# Draw and display the corners
+		cv2.drawChessboardCorners(img, patternSize, corners, ret)
+		
+	cv2.imshow('img',img)
+	cv2.waitKey(1)
 
+cv2.waitKey(10000)
 cv2.destroyAllWindows()
